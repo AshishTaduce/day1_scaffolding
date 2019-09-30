361d4fa16623d1568526a607670bf2314da74d7e
AshishTaduce <ashishteddy2@gmail.com>
2019-09-30 12:29:40 +0530

    Finished Day 1 scaffolding Stuff

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,29 @@ class Student {
 }
 
 void main() {
+  Image image = Image(
+    image: NetworkImage('https://nashuproar.org/wp-content/uploads/2017/09/what-is-a-meme.jpg'),
+  );
+
+
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World',
+      home: Scaffold(
+        appBar: AppBar(
+          //title: Text ('Hello World'),
+          title : Center(child: Icon(Icons.home)),
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://nashuproar.org/wp-content/uploads/2017/09/what-is-a-meme.jpg'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text ('Skrit'),
+          onPressed: () {
+            print("Ta TA Ta TA!");
+          },
         ),
       ),
     ),
